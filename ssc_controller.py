9858965e73e7a36bb76f950d921df8893347956b
Sarah Johnson <sozzifer@gmail.com>
2022-08-29 15:08:00 +0100

    Initial commit

---
+++ ssc_controller.py
@@ -0,0 +1,87 @@
+from dash import Input, Output, exceptions
+import plotly.graph_objects as go
+import numpy as np
+import scipy.stats as stat
+from ssc_view import app
+from ssc_model import stat_colours
+
+
+@app.callback(
+    Output("t-dist-fig", "figure"),
+    Output("output", "style"),
+    Output("current-mu", "children"),
+    Output("current-sigma", "children"),
+    Output("current-nu", "children"),
+    Output("current-alpha", "children"),
+    Output("conf-int", "children"),
+    Output("nu", "disabled"),
+    Output("alpha", "disabled"),
+    Output("sr-t", "children"),
+    Input("submit", "n_clicks"),
+    Input("nu", "value"),
+    Input("alpha", "value"),
+    Input("mu", "value"),
+    Input("sigma", "value"),
+    prevent_initial_call=True
+)
+def update_graph(n_clicks, nu, alpha, mu, sigma):
+    if n_clicks is None or mu is None or sigma is None or nu is None:
+        raise exceptions.PreventUpdate
+    else:
+        x = np.linspace(stat.t.ppf(0.0001, nu, mu, sigma),
+                        stat.t.ppf(0.9999, nu, mu, sigma),
+                        10000)
+        t_x = stat.t.pdf(x, nu, mu, sigma)
+        conf_int = stat.t.interval(alpha, nu, mu, sigma)
+        t1 = round(conf_int[0], 3)
+        t2 = round(conf_int[1], 3)
+        alpha_1tail = 1 - ((1 - alpha)/2)
+        lower_ci = np.linspace(stat.t.ppf(0.0001, nu, mu, sigma),
+                               stat.t.ppf(1-alpha_1tail, nu, mu, sigma),
+                               10000)
+        t_pdf1 = stat.t.pdf(lower_ci, nu, mu, sigma)
+        upper_ci = np.linspace(stat.t.ppf(alpha_1tail, nu, mu, sigma),
+                               stat.t.ppf(0.9999, nu, mu, sigma),
+                               10000)
+        t_pdf2 = stat.t.pdf(upper_ci, nu, mu, sigma)
+        sr_t = f"Student's t distribution graph with mean {mu}, standard deviation {sigma}, {nu} degrees of freedom and confidence level {alpha*100}%"
+        fig = go.Figure(go.Scatter(x=x,
+                                   y=t_x,
+                                   name="t distribution",
+                                   marker_color=stat_colours["norm"],
+                                   hoverinfo="skip"),
+                                   layout={"margin": dict(t=20, b=10, l=20, r=20),
+                                           "height": 400,
+                                           "font_size": 14})
+        fig.add_trace(go.Scatter(x=lower_ci,
+                                 y=t_pdf1,
+                                 name="Probability",
+                                 marker_color=stat_colours["norm"],
+                                 hoverinfo="skip",
+                                 fill="tozeroy",
+                                 fillcolor=stat_colours["z"]))
+        fig.add_trace(go.Scatter(x=upper_ci,
+                                 y=t_pdf2,
+                                 marker_color=stat_colours["norm"],
+                                 hoverinfo="skip",
+                                 fill="tozeroy",
+                                 fillcolor=stat_colours["z"],
+                                 showlegend=False))
+        fig.add_trace(go.Scatter(x=[stat.t.ppf(1-alpha_1tail, nu, mu, sigma)] * 10,
+                                 y=np.linspace(0, t_pdf1[-1], 10),
+                                 name="Lower CI",
+                                 marker_color=stat_colours["+-1std"],
+                                 marker_opacity=0,
+                                 hovertemplate="Lower CI: %{x:.3f}<extra></extra>"))
+        fig.add_trace(go.Scatter(x=[stat.t.ppf(alpha_1tail, nu, mu, sigma)] * 10,
+                                 y=np.linspace(0, t_pdf2[0], 10),
+                                 name="Upper CI",
+                                 marker_color=stat_colours["+-1std"],
+                                 marker_opacity=0,
+                                 hovertemplate="Upper CI: %{x:.3f}<extra></extra>"))
+    return fig, {"display": "inline"}, mu, sigma, nu, f"{alpha:.0%}", f"({t1}, {t2})", False, False, sr_t
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=8080, dev_tools_ui=False)
+    # app.run(debug=True)
---
+++ ssc_view.py
@@ -0,0 +1,127 @@
+from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
+from ssc_model import create_blank_fig
+
+app = Dash(__name__,
+           title="Student's t distribution",
+           update_title=None,
+           external_stylesheets=[dbc.themes.BOOTSTRAP],
+           meta_tags=[{"name": "viewport",
+                       "content": "width=device-width,\
+                                   initial-scale=1.0,\
+                                   maximum-scale=1.0"
+                       }]
+           )
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody(children=[
+                    "This tool will allow you to view the Student's t distribution curve for different confidence levels and degrees of freedom. To display the distribution, enter values for the mean and standard deviation and click ",
+                    html.Span("Set mean and SD", className="bold-p"),
+                    ". Adjust the degrees of freedom and confidence level to see how this affects the confidence interval.",
+                    html.Br(),
+                    html.Br(),
+                    "Pay particular attention to how the scales of the x and y axes change when you enter different values for the mean, standard deviation, degrees of freedom, and confidence level."
+                ])
+            ]),
+            dbc.Card([
+                dbc.CardBody(children=[
+                    html.H4("Results"),
+                    html.Div([
+                        html.P(children=[
+                            html.Span("Mean: ", className="bold-p"),
+                            html.Span(id="current-mu"),
+                            html.Span("Standard deviation: ", className="bold-p", style={"margin-left":"10px"}),
+                            html.Span(id="current-sigma")
+                        ]),
+                        html.P(children=[
+                            html.Span("Degrees of freedom: ", className="bold-p"),
+                            html.Span(id="current-nu"),
+                            html.Span(
+                                "Confidence level: ", className="bold-p", style={"margin-left": "10px"}),
+                            html.Span(id="current-alpha")
+                        ]),
+                        html.P(children=[
+                            html.Span("Confidence interval: ", className="bold-p"),
+                            html.Span(id="conf-int")
+                        ])
+                    ], id="output", style={"display": "none"}, **{"aria-live": "polite"})
+                ])
+            ])
+        ], xs=12, sm=12, md=6, lg=6, xl=6),
+        dbc.Col([
+            html.Div([
+                dcc.Graph(id="t-dist-fig",
+                          figure=create_blank_fig(),
+                          config={"displayModeBar": False})
+            ], role="img"),
+            html.Div(id="sr-t",
+                     children=[],
+                     className="sr-only",
+                     **{"aria-live": "polite"})
+        ], xs=12, sm=12, md=6, lg=6, xl=6)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Label("Mean",
+                       html_for="mean",
+                       className="label"),
+            dbc.Input(id="mu",
+                      name="mean",
+                      value=0,
+                      type="number",
+                      required=True,
+                      debounce=True),
+            dbc.Label("Standard deviation",
+                       className="label",
+                       html_for="standard-deviation"),
+            dbc.Input(id="sigma",
+                      name="standard-deviation",
+                      value=1,
+                      min=0.1,
+                      type="number",
+                      required=True,
+                      debounce=True),
+            html.Div([
+                dbc.Button(id="submit",
+                           n_clicks=0,
+                           children="Set mean and SD",
+                           class_name="button")
+            ], className="d-flex justify-content-center"),
+        ], xs=12, sm=12, md=4, lg=4, xl=4),
+        dbc.Col([
+            dbc.Label("Degrees of freedom",
+                       className="label",
+                       html_for="nu"),
+            dcc.Slider(id="nu",
+                       value=10,
+                       min=1,
+                       max=40,
+                       step=1,
+                       marks={1: {"label": "1"},
+                              5: {"label": "5"},
+                              10: {"label": "10"},
+                              20: {"label": "20"},
+                              30: {"label": "30"},
+                              40: {"label": "40"}},
+                       disabled=True),
+            dbc.Label("Confidence level",
+                       className="label",
+                       html_for="alpha"),
+            dcc.Slider(id="alpha",
+                       value=0.95,
+                       min=0.8,
+                       max=0.99,
+                       step=0.01,
+                       marks={0.8: {"label": "80%"},
+                              0.85: {"label": "85%"},
+                              0.9: {"label": "90%"},
+                              0.95: {"label": "95%"},
+                              0.99: {"label": "99%"}},
+                       disabled=True),
+            html.Br()
+        ], xs=12, sm=12, md=8, lg=8, xl=8)
+    ])
+], fluid=True)
